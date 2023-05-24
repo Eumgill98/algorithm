19b1c542691a6cdfa1f91846e79c546d2aa67663
wjdghcks6735 <wjdghcks02@gmail.com>
2023-05-24 19:35:33 +0900

    feat : 2023-05-24 Baekjoon DP 2225번 : 합분해

---
+++ baekjoon/Dp/2225번 풀이.py
@@ -0,0 +1,10 @@
+n, k = map(int,input().split())
+
+dp = [[1]*(n+1) for f in range(201)]
+
+for i in range(2, k+1):
+    dp[i][1] = i
+    for j in range(2, n+1):
+        dp[i][j] = (dp[i][j-1] + dp[i-1][j])%1000000000
+
+print(dp[k][n])
