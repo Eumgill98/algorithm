aa239915785051f9fcdef1ccdfba31abd4a8d64c
unknown <wjdghcks02@gmail.com>
2023-08-03 18:35:43 +0900

    feat: 2023-08-03 Baekjoon - Heapq 15903번 : 카드합체 놀이

---
+++ baekjoon/Heapq/15903번 풀이.py
@@ -0,0 +1,17 @@
+import heapq
+
+n, m = map(int, input().split())
+heap = list(map(int, input().split()))
+
+heapq.heapify(heap)
+
+for _ in range(m):
+    a = heapq.heappop(heap)
+    b = heapq.heappop(heap)
+
+    tmp = a+b
+    for _ in range(2):
+        heapq.heappush(heap, tmp)
+
+print(heap)
+print(sum(heap))
