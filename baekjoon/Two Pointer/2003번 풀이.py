394872f3c4a97a2510a3ef3047588171825b4b9c
unknown <wjdghcks02@gmail.com>
2023-09-01 20:36:43 +0900

    feat: 2023-09-01 Baekjoon - Two pointer 1940, 2003 번 풀이

---
+++ baekjoon/Two Pointer/2003번 풀이.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+array = list(map(int, input().split()))
+
+left = 0
+right = 1
+result = 0
+
+while right <= n and left <= right:
+    temp = sum(array[left:right])
+
+    if temp == m:
+        result += 1
+        left+=1
+    elif temp > m:
+        left += 1
+    else:
+        right += 1
+
+
+print(result)
