f0c741a57d6dd26ad106cd92fad4a32a3608e5c2
wjdghcks6735 <wjdghcks02@gmail.com>
2023-09-05 17:37:25 +0900

    feat: 2023-09-05 Beakjoon-DP : 5557번 1학년 풀이

---
+++ baekjoon/Dp/5557번 풀이.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+dp = [[0]*21 for _ in range(n)]
+array = list(map(int, input().split()))
+
+
+dp[0][array[0]] = 1
+
+for i in range(1, n-1):
+    for j in range(21):
+        if dp[i-1][j]:
+
+            if 0 <= j + array[i] <= 20:
+                dp[i][j+array[i]] += dp[i-1][j]
+
+            if 0 <= j - array[i] <= 20:
+                dp[i][j-array[i]] += dp[i-1][j]
+
+print(dp[n-2][array[-1]])
