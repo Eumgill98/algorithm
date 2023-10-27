4ad5ae3c27340c6006cae231a4d989a85188bdaa
unknown <wjdghcks02@gmail.com>
2023-10-27 18:53:28 +0900

    feat: 2023-10-27 Baekjoon - DP Maximum Subaaray 풀이

---
+++ baekjoon/Dp/10211번 풀이.py
@@ -0,0 +1,17 @@
+import sys
+
+input = sys.stdin.readline
+
+case =int(input())
+
+for _ in range(case):
+    n = int(input())
+    array = list(map(int, input().split()))
+
+    dp = [0] * n
+    dp[0] = array[0]
+    for i in range(1, n):
+        dp[i] = max(dp[i-1] + array[i], array[i])
+
+    print(max(dp))
+        
