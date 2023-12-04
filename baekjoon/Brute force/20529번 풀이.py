7b9feb484ee2aeb97aaf60fbed7e3a9d7932373e
wjdghcks6735 <wjdghcks02@gmail.com>
2023-12-04 17:35:46 +0900

    feat: add 2023-12-04 Baekjoon - Brute force 20529번 : 가장 가까운 세사람의 심리적 거리

---
+++ baekjoon/Brute force/20529번 풀이.py
@@ -0,0 +1,35 @@
+import sys
+from itertools import combinations
+
+input = sys.stdin.readline
+
+def mbti_dist(a, b):
+    dist = 0
+    for i, j in zip(a, b):
+        if i != j:
+            dist += 1
+    return dist
+
+T = int(input())
+for _ in range(T):
+    N = int(input())
+    MBTI = list(map(str, input().split()))
+
+    if N > 32:
+        print(0)
+    else:
+        res = 13
+        case = combinations(MBTI, 3)
+
+        for a, b, c in case:
+            dist = 0
+            dist += mbti_dist(a,b)
+            dist += mbti_dist(b,c)
+            dist += mbti_dist(a,c)
+
+            res = min(res, dist)
+
+        print(res)
+
+
+        
