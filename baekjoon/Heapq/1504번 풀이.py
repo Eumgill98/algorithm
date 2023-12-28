bfb660251157d2a39319af815e27a892dd58326e
wjdghcks6735 <wjdghcks02@gmail.com>
2023-12-28 19:11:43 +0900

    faet: 2023-12-28 Baekjoon - Heapq 1504번 : 특정한 최단 경로 풀이

---
+++ baekjoon/Heapq/1504번 풀이.py
@@ -0,0 +1,49 @@
+import heapq
+import sys
+
+input = sys.stdin.readline
+INF = 1e9
+
+N, E = map(int, input().split())
+graph = [[] for _ in range(N+1)]
+
+for _ in range(E):
+    a, b, c = map(int, input().split())
+    graph[a].append((b, c))
+    graph[b].append((a, c))
+
+def dijkstra(start):
+    distance = [INF] * (N + 1)
+
+    q = []
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
+
+    while q:
+        dist, now = heapq.heappop(q)
+
+        if distance[now] < dist:
+            continue
+
+        for i in graph[now]:
+            cost = dist + i[1]
+
+            if distance[i[0]] > cost:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
+
+    return distance
+
+v1, v2 = map(int, input().split())
+original_distance = dijkstra(1)
+v1_distance = dijkstra(v1)
+v2_distance = dijkstra(v2)
+
+v1_path = original_distance[v1] + v1_distance[v2] + v2_distance[N]
+v2_path = original_distance[v2] + v2_distance[v1] + v1_distance[N]
+
+result = min(v1_path, v2_path)
+print(result if result < INF else -1)
+
+    
+
